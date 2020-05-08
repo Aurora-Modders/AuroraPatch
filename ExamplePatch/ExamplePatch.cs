23130d9225f167f5a7481ec4621a0140c9ad5dbc
01010100b <caveman1917@hotmail.com>
2020-05-08 19:18:39 +0200

    clear up example patch

--- ExamplePatch/ExamplePatch.cs
+++ ExamplePatch/ExamplePatch.cs
@@ -5,21 +5,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ExamplePatch
 {
     public class ExamplePatch : AuroraPatch.Patch
     {
-        internal static Form TacticalMap { get; private set; } = null;
+        private Form TacticalMap { get; set; } = null;
 
         protected override void Start(Form map)
         {
+            // save a reference to the tactical map
             TacticalMap = map;
 
+            // invoke arbitrary code on Aurora's UI thread
             var action = new Action(() => MessageBox.Show("Example patch loaded!"));
             InvokeOnUIThread(action);
 
+            // Harmony support
             var harmony = new Harmony("some.id");
             var method = (MethodBase)map.GetType().Assembly.GetTypes()
                 .Single(type => type.Name == "Economics")
@@ -27,6 +31,15 @@ namespace ExamplePatch
                 .Single(m => m.Name == "InitializeComponent");
 
             harmony.Patch(method, new HarmonyMethod(((Action)PatchMethod).Method));
+
+            // Patch.Start is run on its own background thread, no need to return
+            while (true)
+            {
+                Thread.Sleep(15 * 60 * 1000);
+
+                action = new Action(() => MessageBox.Show("Another 15 minutes have passed"));
+                InvokeOnUIThread(action);
+            }
         }
 
         private static void PatchMethod()
