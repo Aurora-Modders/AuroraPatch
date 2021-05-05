f5b9f9710e3146da1b8aa4e9c8a15b008f704139
Edouard Poitras <edouardpoitras@gmail.com>
2021-05-05 09:29:49 -0400

    Now creating Patches directory if not exists and cleaned up imports

--- AuroraPatch/Patch.cs
+++ AuroraPatch/Patch.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace AuroraPatch
 {
