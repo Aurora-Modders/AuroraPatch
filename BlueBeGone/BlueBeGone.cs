b914c06ed9f9c4bf18f0dafa5a39300955c861f2
Edouard Poitras <edouardpoitras@gmail.com>
2021-05-07 12:31:42 -0400

    Added UIControls and BlueBeGone patches

---
+++ BlueBeGone/BlueBeGone.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using System.Collections.Generic;
+
+using HarmonyLib;
+
+namespace BlueBeGone
+{
+    public class BlueBeGone : AuroraPatch.Patch
+    {
+        public override string Description => "Simplest possible theme that swaps all blue with black.";
+        public override IEnumerable<string> Dependencies => new[] { "UIControls" };
+
+        protected override void Load(Harmony harmony)
+        {
+            UIControls.UIControls.GlobalColorSwaps.Add(Color.FromArgb(0, 0, 64), Color.Black);
+        }
+    }
+}
